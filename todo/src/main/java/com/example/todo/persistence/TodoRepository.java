bf912c347f77a8e7b0f0d4c80f24a359a38d958a
kahyun0255 <kkh9946@naver.com>
2024-05-27 15:40:41 +0900

    책 내용 완료

---
+++ todo/src/main/java/com/example/todo/persistence/TodoRepository.java
@@ -0,0 +1,14 @@
+package com.example.todo.persistence;
+
+import com.example.todo.model.TodoEntity;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+
+    @Query("select t from TodoEntity t where t.userId=?1")
+    List<TodoEntity>findByUserId(String userId);
+}
